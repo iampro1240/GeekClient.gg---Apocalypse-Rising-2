89d3cf26605655642783cf4cf45a44cfcb335396
iampro1240 <wadawdw7@gmail.com>
2026-04-08 04:55:42 -0400

    Create services.lua

---
+++ services.lua
@@ -0,0 +1,55 @@
+local lib = {}
+local camera = workspace.CurrentCamera
+lib.__index = lib
+
+
+function lib:CloneFunction(obj)
+    return clonefunction(obj)
+end
+
+
+function lib:CloneReference(obj)
+    return clonefunction(obj)
+end
+
+
+function lib:GetService(obj)
+    return game:GetService(obj)
+end
+
+
+function lib:wtvp(obj)
+    return camera:WorldToViewportPoint(obj)
+end
+
+
+function lib:wtsp(obj)
+    return camera:WorldToScreenPoint(obj)
+end
+
+
+function lib:framelimit(deltatime)
+    local rendertime = 0   
+    rendertime += deltatime
+	if (rendertime < 1 / 60) then
+		return
+	end
+	rendertime = 0
+end
+
+
+function lib:GarbageCollection(func)
+for i, v in next, getgc(true) do
+if typeof(v) ~= "function" then
+continue
+end
+local info = debug.getinfo(v)
+if info.name == func then
+   return v
+  end
+ end
+end
+
+
+
+return lib
