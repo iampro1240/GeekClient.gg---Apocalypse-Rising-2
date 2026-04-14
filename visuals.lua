bc424960dcbbb551e6d9635215813fed4bb0618a
iampro1240 <wadawdw7@gmail.com>
2026-04-14 05:18:57 -0400

    Update visuals.lua

--- visuals.lua
+++ visuals.lua
@@ -896,7 +896,7 @@ esp.connection = RunService.PreRender:Connect(function(deltatime)
    
     do
         UI.PlayerName.Text = cache.name
-        UI.PlayerName.Size = dim2(0.75, 0 * distancemath + .1, 0, .8 / distancemath / 8 - 22)
+        UI.PlayerName.Size = dim2(0.75, 0 * distancemath + .1, 0, .8 / distancemath / 40)
         UI.PlayerName.Position = dim2(0.13, 0, .12, 0)
         UI.PlayerName.Visible = lib2.flags["Names"] and os
         UI.PlayerName.TextColor3 = returnflagcolor("Name_Color")
